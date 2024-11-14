2de172b14566c87a9a399bc6d7a0b6930fd9e90f
lucasarabi <lucaarab01@gmail.com>
2024-11-13 22:09:28 -0500

    Share code

--- main.cpp
+++ main.cpp
@@ -1,34 +1,40 @@
+#include <iostream>
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
-    GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 600
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+void renderGame(GLFWwindow* window) {
+    float vertices[] = { -0.5f, -0.5f, 0.0f,
+                          0.5f, -0.5, 0.0f,
+                          0.0f, 0.5f, 0.0f };
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+}
+
+void setup() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+int main() {
+    setup();
+    GLFWwindow* window = glfwCreateWindow(WINDOW_HEIGHT, WINDOW_WIDTH, "Open GL Learning Material", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glViewport(0, 0, WINDOW_HEIGHT, WINDOW_WIDTH);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
+    while(!glfwWindowShouldClose(window)) {
+        renderGame(window);
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
